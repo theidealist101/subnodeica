e3bfcf40cedc3a1acd826c25cb796284ef15f564
theidealist <theidealistmusic@gmail.com>
2024-06-13 12:15:19 +0100

    seamoth now has visible health and can be damaged

--- mods/sub_vehicles/init.lua
+++ mods/sub_vehicles/init.lua
@@ -1,3 +1,30 @@
+sub_vehicles = {}
+
+--HUDs for vehicle health and power
+local health_huds = {}
+
+function sub_vehicles.add_huds(player, vehicle)
+    health_huds[player] = player:hud_add({
+        hud_elem_type = "statbar",
+        position = {x=0.5, y=1},
+        size = {x=24, y=24},
+        alignment = {x=-1, y=-1},
+        offset = {x=-265, y=-155},
+        text = "sub_vehicles_health_hud.png",
+        number = 20*vehicle:get_hp()/vehicle:get_properties().hp_max
+    })
+end
+
+function sub_vehicles.update_huds(player)
+    local vehicle = player:get_attach()
+    player:hud_change(health_huds[player], "number", 20*vehicle:get_hp()/vehicle:get_properties().hp_max)
+end
+
+function sub_vehicles.remove_huds(player)
+    player:hud_remove(health_huds[player])
+    health_huds[player] = nil
+end
+
 local path = minetest.get_modpath("sub_vehicles").."/"
 dofile(path.."seamoth.lua")
 dofile(path.."constructor.lua")
--- mods/sub_vehicles/seamoth.lua
+++ mods/sub_vehicles/seamoth.lua
@@ -5,6 +5,57 @@ local GRAVITY = -1
 local FRICTION = 0.3
 
 --Seamoth, one-manned submersible, small and unarmored but easily manoevrable and extendable
+local function seamoth_on_step(self, dtime, moveresult)
+    local accel = vector.zero()
+    local in_air = minetest.get_node(mobkit.get_node_pos(self.object:get_pos())).name == "air"
+
+    --check there is someone driving
+    local driver = self.object:get_children()[1]
+    if driver then
+        local controls = driver:get_player_control()
+        if controls.aux1 then
+            driver:set_detach()
+            driver:set_eye_offset(vector.zero())
+            sub_vehicles.remove_huds(driver)
+            --based on how it's done in Minetest Game's boats mod
+            local dismount_pos = driver:get_pos()
+            dismount_pos.y = dismount_pos.y+1
+            minetest.after(0.1, function() driver:set_pos(dismount_pos) end)
+        else
+            sub_vehicles.update_huds(driver)
+            local rot = vector.new(-driver:get_look_vertical(), driver:get_look_horizontal(), 0)
+            self.object:set_rotation(rot)
+
+            --apply force based on controls
+            if not in_air then
+                local forward = ACCEL*mobkit.rot_to_dir(rot)
+                local sideways = vector.new(forward.z, 0, -forward.x)
+                if controls.up then accel = accel+forward end
+                if controls.down then accel = accel-forward*0.5 end
+                if controls.right then accel = accel+sideways end
+                if controls.left then accel = accel-sideways end
+                if controls.jump then accel.y = accel.y+ACCEL end
+                if controls.sneak then accel.y = accel.y-ACCEL end
+            end
+        end
+    end
+
+    --do some physics
+    if accel == vector.zero() then
+        accel = -self.object:get_velocity()
+        if vector.length(accel) > FRICTION then accel = FRICTION*vector.normalize(accel) end
+        if in_air then
+            accel.y = accel.y+GRAVITY
+        end
+    end
+
+    --update motion
+    local vel = self.object:get_velocity()+accel
+    if vector.length(vel) > MAX_SPEED then vel = MAX_SPEED*vector.normalize(vel) end
+    self.object:set_velocity(vel)
+    sub_nav.move_waypoint(self.waypoint, self.object:get_pos())
+end
+
 minetest.register_entity("sub_vehicles:seamoth", {
     initial_properties = {
         visual = "mesh",
@@ -12,10 +63,16 @@ minetest.register_entity("sub_vehicles:seamoth", {
         mesh = "seamoth.obj",
         textures = {"seamoth.png"},
         collisionbox = {-1, -1, -1, 1, 1, 1},
-        physical = true
+        physical = true,
+        hp_max = 40
     },
     breathable = true,
     on_activate = function (self, staticdata)
+        self.object:set_armor_groups({
+            normal = 100,
+            gas = 100,
+            fire = 100
+        })
         if staticdata and staticdata ~= "" then
             self.waypoint = tonumber(staticdata)
         else
@@ -32,55 +89,9 @@ minetest.register_entity("sub_vehicles:seamoth", {
         user:set_look_vertical(-rot.x)
         user:set_look_horizontal(rot.y)
         user:set_eye_offset(vector.new(0, -12, 0))
+        sub_vehicles.add_huds(user, self.object)
     end,
-    on_step = function (self, dtime, moveresult)
-        local accel = vector.zero()
-        local in_air = minetest.get_node(mobkit.get_node_pos(self.object:get_pos())).name == "air"
-
-        --check there is someone driving
-        local driver = self.object:get_children()[1]
-        if driver then
-            local controls = driver:get_player_control()
-            if controls.aux1 then
-                driver:set_detach()
-                driver:set_eye_offset(vector.zero())
-                --based on how it's done in Minetest Game's boats mod
-                local dismount_pos = driver:get_pos()
-                dismount_pos.y = dismount_pos.y+1
-                minetest.after(0.1, function() driver:set_pos(dismount_pos) end)
-            else
-                local rot = vector.new(-driver:get_look_vertical(), driver:get_look_horizontal(), 0)
-                self.object:set_rotation(rot)
-
-                --apply force based on controls
-                if not in_air then
-                    local forward = ACCEL*mobkit.rot_to_dir(rot)
-                    local sideways = vector.new(forward.z, 0, -forward.x)
-                    if controls.up then accel = accel+forward end
-                    if controls.down then accel = accel-forward*0.5 end
-                    if controls.right then accel = accel+sideways end
-                    if controls.left then accel = accel-sideways end
-                    if controls.jump then accel.y = accel.y+ACCEL end
-                    if controls.sneak then accel.y = accel.y-ACCEL end
-                end
-            end
-        end
-
-        --do some physics
-        if accel == vector.zero() then
-            accel = -self.object:get_velocity()
-            if vector.length(accel) > FRICTION then accel = FRICTION*vector.normalize(accel) end
-            if in_air then
-                accel.y = accel.y+GRAVITY
-            end
-        end
-
-        --update motion
-        local vel = self.object:get_velocity()+accel
-        if vector.length(vel) > MAX_SPEED then vel = MAX_SPEED*vector.normalize(vel) end
-        self.object:set_velocity(vel)
-        sub_nav.move_waypoint(self.waypoint, self.object:get_pos())
-    end,
+    on_step = seamoth_on_step,
     get_staticdata = function (self)
         return tostring(self.waypoint)
     end
