ea238c294c6f182bdda2d5688016b59d61417b0b
theidealist <theidealistmusic@gmail.com>
2024-07-09 13:45:04 +0100

    added hatches to the lifepod

--- mods/sub_initial_stuff/init.lua
+++ mods/sub_initial_stuff/init.lua
@@ -58,6 +58,35 @@ minetest.register_node("sub_initial_stuff:lifepod_ladder", {
     climbable = true,
 })
 
+local function hatch_on_rightclick(pos, _, clicker, _, pointed)
+    if not pointed.type == "node" then return end
+    local dir = pos-pointed.above
+    if dir == vector.new(0, -1, 0) then dir = vector.new(0, -2, 0) end
+    dir.y = dir.y-0.5
+    clicker:set_pos(pos+dir)
+end
+
+minetest.register_node("sub_initial_stuff:hatch", {
+    description = "Hatch",
+    drawtype = "nodebox",
+    node_box = {
+        type = "fixed",
+        fixed = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5} --imitates glasslike while being wallmounted
+    },
+    tiles = {
+        "sub_initial_stuff_hatch.png",
+        "sub_initial_stuff_hatch.png",
+        "blank.png",
+        "blank.png",
+        "blank.png",
+        "blank.png"
+    },
+    paramtype = "light",
+    paramtype2 = "wallmounted",
+    sunlight_propagates = true,
+    on_rightclick = hatch_on_rightclick
+})
+
 minetest.register_on_newplayer(function(player)
     player:set_pos(vector.new(0, 1, -1))
     minetest.place_schematic(vector.zero(), minetest.get_modpath("sub_initial_stuff").."/schems/lifepod5.mts", nil, nil, true, "place_center_x, place_center_z")
