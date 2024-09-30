321b81a1d227aca514339a1de4db0dddf21d28a7
theidealist <theidealistmusic@gmail.com>
2024-09-30 12:40:05 +0100

    added pathfinder which can place little light-emitting nodes and pick them up, consuming power

--- mods/sub_crafts/light_tools.lua
+++ mods/sub_crafts/light_tools.lua
@@ -7,6 +7,7 @@ local flare_defs = {
     drawtype = "torchlike",
     tiles = {"sub_crafts_flare.png"},
     selection_box = {type="fixed", fixed={-0.25, -0.5, -0.25, 0.25, -0.375, 0.25}},
+    walkable = false,
     light_source = 12,
     on_rightclick = function (pos, node, user, itemstack)
         local out = ItemStack("sub_crafts:item_flare")
@@ -54,7 +55,7 @@ minetest.register_tool("sub_crafts:item_flare", {
     on_place = function (itemstack, user, pointed)
         local nodename = minetest.get_node(pointed.above).name
         if pointed.above.y-pointed.under.y ~= 1 or minetest.registered_nodes[nodename].drawtype ~= "airlike" then return end
-        minetest.place_node(pointed.above, {name=nodename == "air" and "sub_crafts:air_flare" or sub_core.get_waterlogged("sub_crafts:flare", nodename)})
+        minetest.set_node(pointed.above, {name=nodename == "air" and "sub_crafts:air_flare" or sub_core.get_waterlogged("sub_crafts:flare", nodename)})
         minetest.get_meta(pointed.above):set_int("wear", itemstack:get_wear())
         itemstack:take_item()
         return itemstack
@@ -86,6 +87,7 @@ local light_stick_defs = {
     tiles = {"sub_crafts_light_stick.png"},
     use_texture_alpha = "clip",
     selection_box = {type="fixed", fixed={-0.125, -1.5, -0.125, 0.125, 0.25, 0.125}},
+    walkable = false,
     light_source = 14,
     on_rightclick = function (pos, node, user, itemstack)
         minetest.set_node(pos, {name=minetest.registered_nodes[node.name]._water_equivalent or "air"})
@@ -115,4 +117,59 @@ sub_crafts.register_craft({
     subcategory = "tools",
     output = {"sub_crafts:item_light_stick"},
     recipe = {"sub_crafts:battery", "sub_core:titanium", "sub_crafts:glass"}
+})
+
+--Pathfinder tool, used to show the way out of places
+local path_defs = {
+    description = "Path Node",
+    drawtype = "plantlike",
+    tiles = {"sub_crafts_path_node.png"},
+    selection_box = {type="fixed", fixed={-0.125, -0.5, -0.125, 0.125, -0.375, 0.125}},
+    use_texture_alpha = "blend",
+    walkable = false,
+    paramtype = "light",
+    sunlight_propagates = true,
+    light_source = 7,
+    groups = {path_node=1}
+}
+
+minetest.register_node("sub_crafts:air_path_node", path_defs)
+sub_core.register_waterloggable("sub_crafts:path_node", path_defs)
+
+minetest.register_tool("sub_crafts:pathfinder", {
+    description = "Pathfinder",
+    inventory_image = "sub_crafts_pathfinder.png",
+    on_place = function (itemstack, user, pointed)
+        local nodename = minetest.get_node(pointed.above).name
+        local node_under = minetest.get_node(pointed.under).name
+        if minetest.get_item_group(node_under, "path_node") > 0 then
+            minetest.set_node(pointed.under, {name=minetest.registered_nodes[node_under]._water_equivalent or "air"})
+            return
+        end
+        if pointed.above.y-pointed.under.y ~= 1 or minetest.registered_nodes[nodename].drawtype ~= "airlike" then return end
+        local wear = itemstack:get_wear()
+        if wear >= 65535 then return end
+        minetest.set_node(pointed.above, {name=nodename == "air" and "sub_crafts:air_path_node" or sub_core.get_waterlogged("sub_crafts:path_node", nodename)})
+        wear = math.min(math.max(math.round(wear+327.5), 0), 65535) --327.5 is approximately 65535/200
+        itemstack:set_wear(wear)
+        return itemstack
+    end,
+    on_use = sub_crafts.switch_battery,
+    _hovertext = function (itemstack, user, pointed)
+        local out = itemstack:get_wear() >= 65535 and "Switch battery (LMB)" or ""
+        if pointed.type ~= "node" then return out end
+        local nodename = minetest.get_node(pointed.under).name
+        if minetest.get_item_group(nodename, "path_node") > 0 then
+            return "Pick up path node (RMB)\n"..out
+        else
+            return "Place path node (RMB)\n"..out
+        end
+    end
+})
+
+sub_crafts.register_craft({
+    category = "personal",
+    subcategory = "tools",
+    output = {"sub_crafts:pathfinder"},
+    recipe = {"sub_core:creepvine_seeds 2", "sub_crafts:copper_wire", "sub_core:titanium"}
 })
